bdf62a915b0e455fa4a099cd2366a9bd1c455634
Nurzhanov-Bekzat <bekzatnurzhanov888@gmail.com>
2023-04-12 12:25:19 +0600

    chabge-inut

--- hw-4/src/Components/Modal/Modal.jsx
+++ hw-4/src/Components/Modal/Modal.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Input } from '../UI/Input/Input'
 import Button from '../UI/Button/Button'
+import "./Modal.css"
 
 export const Modal = ({addImgUrl,addRating,addTitle,imgUrl,title,rating}) => {
     console.log(rating)
   return (
-    <div>
+    <div className='contain'>
+      <label htmlFor="">imgUrl</label>
         <Input onChange={addImgUrl} value={imgUrl} />
+        <label htmlFor="">Title</label>
         <Input onChange={addTitle} value={title} />
+        <label htmlFor="">rating</label>
         <Input onChange={addRating} value={rating} />
-<div>
+<div className='btnnn'>
 <Button>Cancel</Button>
         <Button>Add</Button>
 </div>
--- hw-4/src/Components/UI/Input/Input.jsx
+++ hw-4/src/Components/UI/Input/Input.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import "./input.css"
+
 export const Input = ({onChange,value}) => {
   return (
     <input onChange={onChange} value={value} type="text" />
